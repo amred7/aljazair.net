c5f908e492633e011ddfd0cdb3856a66ae4d8edd
reda akachat <reda@akachat.net>
2025-11-11 20:50:19 -0500

    amplify yml fix v0.3

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource.js";
 import { storage } from "./storage/resource.js";
 
 defineBackend({
-  auth,
   storage
 });
