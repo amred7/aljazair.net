788350053993c90475575608eca2a94870dbaf4a
reda akachat <reda@akachat.net>
2025-11-11 22:01:58 -0500

    added ampligy v0.5

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1> Coucou de main</h1>
-        <Authenticator>
+        <Authenticator signUpAttributes={["family_name"]} >
           {({ signOut, user }) => (
             <div>
               <h1>Hello {user?.username}</h1>
